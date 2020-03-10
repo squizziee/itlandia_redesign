723ae41cf711fcf3b8bc5ee39eaaff01fcc4265d
squizziee <3925181@gmail.com>
2020-03-10 20:01:04 +0300

    Social buttons functionality added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-// Soon...
+$(".social_btn_wrapper").on("click", function() {
+    $(this).toggleClass("rubberBandAnimated");
+});
+
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 1500 );
+}
