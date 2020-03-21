6797afd04b24c2fd5d56217f300484adb71efc50
squizziee <3925181@gmail.com>
2020-03-21 14:19:20 +0300

    Adding main information page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ $(".social_btn_wrapper").on("click", function() {
 function delay (URL) {
     setTimeout( function() { window.location = URL }, 1500 );
 }
+
+var height = $('#header').height();
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > height) {
+        $(".navbar").addClass("fixed");
+    } else {
+        $(".navbar").removeClass("fixed");
+    }
+});
