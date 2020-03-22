0c623f4294af692109b2dc887fb0607e59b2056a
squizziee <3925181@gmail.com>
2020-03-22 15:25:11 +0300

    Upgrade navbar functionality

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ $(".social_btn_wrapper").on("click", function() {
     $(this).toggleClass("rubberBandAnimated");
 });
 
+$(".navbar_option").on("click", function() {
+    if (!($(this).hasClass("navbar_option_active"))) {
+        $(".navbar_option").removeClass("navbar_option_active_menu");
+        $(".navbar_option").removeClass("navbar_option_active");
+    }
+    $(this).toggleClass("navbar_option_active_menu");
+    $(this).toggleClass("navbar_option_active");
+});
+
 function delay (URL) {
     setTimeout( function() { window.location = URL }, 1500 );
 }
